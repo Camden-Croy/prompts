ca9917f9f56cdf774d17ee0c21dfe4bc3e62eda3
Camden Croy <cam.croytn@gmail.com>
2023-10-15 19:47:11 -0400

    ??

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,16 +7,24 @@ import {useState, useEffect} from 'react';
 import {signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
         <Link href="/" className="flex gap-2 flex-center">
             <Image
-            src="/public/images/logo-color.svg"
+            src="/images/logo-color.svg"
             alt='Promptopia Logo'
-            width={30}
-            height={30}
+            width={60}
+            height={60}
             />
+            <p className='logo_text'>Promptopia</p>
         </Link>
+
+        {/* Mobile Navigation */}
+        <div className='sm:flex hidden'>
+          {isUserLoggedIn ? <div className='flex gap-3 md:gap-5'></div> : <></>}
+        </div>
+
     </nav>
   )
 }
